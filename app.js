9bce09c0157051c45fc70cfb5666af1a1606acb0
Shubhajit612 <shubhajit612@gmail.com>
2023-02-07 11:21:52 +0530

    Adding second file

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-cpnsole.log("I love Git");
+// console.log(document.link);
+
+// console.log(document.getElementById('header-title'));
+
+// var headerTitle = document.getElementById('header-title');
+
+// let header = document.getElementById('main-header');
+
+// headerTitle.textContent = "Hello";
+
+// headerTitle.innerHTML = "<h2>Goodbye</h2>";
+
+// header.style.borderBottom = 'solid 3px #000'
+
+// console.log(headerTitle.textContent);
+
+var items = document.getElementsByClassName('list-group-item');
+
+console.log(items[1]);
+
+items[1].textContent = "Hello 2";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "green";
+
+items.style.backgroundColor = "#f4f4f4";
+
+
+// console.log
