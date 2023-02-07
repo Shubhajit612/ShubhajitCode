2094c352559bd3e9a52151048bdfe8021d9d6907
Shubhajit612 <shubhajit612@gmail.com>
2023-02-07 13:31:45 +0530

    Some Changes

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-// console.log(document.link);
-
-// console.log(document.getElementById('header-title'));
-
-// var headerTitle = document.getElementById('header-title');
-
-// let header = document.getElementById('main-header');
-
-// headerTitle.textContent = "Hello";
-
-// headerTitle.innerHTML = "<h2>Goodbye</h2>";
-
-// header.style.borderBottom = 'solid 3px #000'
-
-// console.log(headerTitle.textContent);
-
 var items = document.getElementsByClassName('list-group-item');
-
+console.log(items);
 console.log(items[1]);
+items[1].textContent = "Hello Brother..!!";
+items[1].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'Green';
 
-items[1].textContent = "Hello 2";
-items[1].style.fontWeight = "bold";
-items[1].style.backgroundColor = "green";
+for(let i=0;i<items.length;i++){
 
-items.style.backgroundColor = "#f4f4f4";
+    items[i].style.fontWeight = "bold";
 
-
-// console.log
+}
