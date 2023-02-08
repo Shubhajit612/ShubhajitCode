2ed20e46c88149a704502a88994275101d8825d9
Shubhajit612 <shubhajit612@gmail.com>
2023-02-08 10:11:40 +0530

    Changes applied..again

--- demo.js
+++ demo.js
@@ -1,11 +1,26 @@
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
 
-items[1].textContent = "Hello Brother..!";
+// items[1].textContent = "Hello Brother..!";
 
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
-for(let i =0 ;i<items.length;i++){
-    items[i].style.fontWeight = "bold";
-}
+// for(let i =0 ;i<items.length;i++){
+//     items[i].style.fontWeight = "bold";
+// }
+
+//queryselector
+
+var p = document.getElementsByClassName('list2');
+
+console.log(p);
+
+p[0].textContent = "Hello Shubhajit..!!";
+p[0].style.fontWeight = 'bold';
+
+var v = document.getElementsByTagName("li");
+
+console.log(v);
+
+v[4].style.backgroundColor = "Yellow";
