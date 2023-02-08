58a6e58303f20e941511b1806d678225dd0d2eb8
Shubhajit612 <shubhajit612@gmail.com>
2023-02-08 09:26:56 +0530

    Added 2 file

---
+++ demo.js
@@ -0,0 +1,11 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+
+items[1].textContent = "Hello Brother..!";
+
+items[2].style.backgroundColor = "green";
+
+for(let i =0 ;i<items.length;i++){
+    items[i].style.fontWeight = "bold";
+}
