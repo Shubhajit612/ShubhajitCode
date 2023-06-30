82fe3bfc0783117e5a072011f4d9b729f1a9b80b
Shubhajit612 <shubhajit612@gmail.com>
2023-06-30 21:15:30 +0530

    Edit wala...

--- demo.js
+++ demo.js
@@ -6,6 +6,8 @@ const myemail = document.getElementById('email');
 
 myForm.addEventListener('submit',onSubmit);
 
+var edit = null;
+
 function onSubmit(e){
 
     e.preventDefault();
@@ -18,22 +20,49 @@ function onSubmit(e){
        email
     };
 
-    axios.post("https://crudcrud.com/api/405b0badced64e069d82ba2dfb61121a/appointmentData",myObj)
-    .then((response)=>{
-      console.log(response);
-      showUserOnScreen(response.data);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+    // axios.post("https://crudcrud.com/api/1f363630f4c04be49a82b2b9adaa2069/appointmentData",myObj)
+    // .then((response)=>{
+    //   // console.log(response);
+    //   showUserOnScreen(response.data);
+    // })
+    // .catch((err)=>{
+    //   console.log(err);
+    // })
 
     // localStorage.setItem(myObj.email,JSON.stringify(myObj));
 
     // showUserOnScreen(myObj);
+
+    if(edit)
+       {
+        delete myObj._id;
+          axios.put(`https://crudcrud.com/api/1f363630f4c04be49a82b2b9adaa2069/appointmentData/${edit}`,myObj)
+          .then((res)=>{
+            // console.log(res);
+            myObj._id = edit;
+
+            showUserOnScreen(myObj);
+         
+          edit = null;
+          })
+          .catch((err)=>{
+            console.log(err);
+          })
+       }
+       else{
+        axios.post('https://crudcrud.com/api/1f363630f4c04be49a82b2b9adaa2069/appointmentData',myObj)
+        .then((response)=>{
+          // console.log(response);
+          showUserOnScreen(response.data);
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+       }
 }
 
 window.addEventListener("DOMContentLoaded",()=>{
-  const data = axios.get("https://crudcrud.com/api/405b0badced64e069d82ba2dfb61121a/appointmentData")
+  const data = axios.get("https://crudcrud.com/api/1f363630f4c04be49a82b2b9adaa2069/appointmentData")
   .then((res)=>{
     console.log(res);
 
@@ -60,19 +89,23 @@ function showUserOnScreen(myObj){
     editChild.type = 'button';
     editChild.value = 'edit';
 
-    editChild.onclick = () =>{
-        localStorage.removeItem(myObj.email);
-        parentEle.removeChild(childEle);
 
-        document.getElementById('name').value = myObj.name;
-        document.getElementById('email').value = myObj.email;
+    editChild.onclick = () =>{
+        // localStorage.removeItem(myObj.email);
+      edit = myObj._id;
+
+      parentEle.removeChild(childEle);
+      document.getElementById('name').value = myObj.name;
+      document.getElementById('email').value = myObj.email;
+
+       
     }
 
 
 
     deleteChild.onclick = () =>{
         // localStorage.removeItem(myObj.email);
-        axios.delete(`https://crudcrud.com/api/405b0badced64e069d82ba2dfb61121a/appointmentData/${myObj._id}`)
+        axios.delete(`https://crudcrud.com/api/1f363630f4c04be49a82b2b9adaa2069/appointmentData/${myObj._id}`)
         .then((res)=>{
           parentEle.removeChild(childEle);
           // console.log(`deleted element is ${res}`);
